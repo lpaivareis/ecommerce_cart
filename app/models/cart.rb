025d8334f36d1f2711ecc878847ad5fd01a3241f
Lucas Felipe <lfelipepaivareis@gmail.com>
2025-01-27 23:09:15 -0300

    Add new methods in `Cart` and add factory

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,16 @@ class Cart < ApplicationRecord
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+
+  def mark_as_abandoned
+    update(last_interaction_at: Time.current)
+  end
+
+  def abandoned?
+    last_interaction_at > 3.hours.ago
+  end
+
+  def remove_if_abandoned
+    destroy if last_interaction_at > 7.days.ago
+  end
 end
