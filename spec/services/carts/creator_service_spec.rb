ce4a09f4e669cd8572e0d75cd6e832d5b05dd88d
Lucas Felipe <lfelipepaivareis@gmail.com>
2025-01-29 00:54:58 -0300

    Fix specs

--- spec/services/carts/creator_service_spec.rb
+++ spec/services/carts/creator_service_spec.rb
@@ -1,67 +1,65 @@
-# spec/services/carts/creator_service_spec.rb
 require 'rails_helper'
 
 RSpec.describe Carts::CreatorService do
   let(:product) { create(:product, price: 10.0) }
   let(:params) { { product_id: product.id, quantity: 2 } }
-  
+
   describe '#call' do
+    subject { described_class.call(params, cart_id) }
+
     context 'when creating a new cart' do
+      let(:cart_id) { nil }
+
       it 'creates a new cart with the product' do
-        result = described_class.call(params)
-        
-        expect(result).to be_success
-        expect(result.data).to be_a(Cart)
-        expect(result.data.cart_items.count).to eq(1)
-        expect(result.data.cart_items.first.quantity).to eq(2)
-        expect(result.data.total_price).to eq(20.0)
+        expect(subject).to be_success
+        expect(subject.data).to be_a(Cart)
+        expect(subject.data.cart_items.count).to eq(1)
+        expect(subject.data.cart_items.first.quantity).to eq(2)
+        expect(subject.data.total_price).to eq(20.0)
       end
     end
 
     context 'when adding to existing cart' do
       let!(:cart) { create(:shopping_cart) }
-      
+      let(:cart_id) { cart.id }
+
       it 'adds new product to existing cart' do
-        result = described_class.call(params, cart.id)
-        
-        expect(result).to be_success
-        expect(result.data.id).to eq(cart.id)
-        expect(result.data.cart_items.count).to eq(1)
+        expect(subject).to be_success
+        expect(subject.data.id).to eq(cart.id)
+        expect(subject.data.cart_items.count).to eq(1)
       end
 
       context 'when product already exists in cart' do
         let!(:cart_item) { create(:cart_item, cart: cart, product: product, quantity: 3) }
 
         it 'updates the quantity of existing product' do
-          result = described_class.call(params, cart.id)
-          
-          expect(result).to be_success
-          expect(result.data.cart_items.count).to eq(1)
-          expect(result.data.cart_items.first.quantity).to eq(5) # 3 + 2
-          expect(result.data.total_price).to eq(50.0) # 5 * 10
+          expect(subject).to be_success
+          expect(subject.data.cart_items.count).to eq(1)
+          expect(subject.data.cart_items.first.quantity).to eq(5)
+          expect(subject.data.total_price).to eq(50.0)
         end
       end
     end
 
     context 'when cart_id is invalid' do
+      let(:cart_id) { 999 }
+
       it 'creates a new cart' do
-        result = described_class.call(params, 999)
-        
-        expect(result).to be_success
-        expect(result.data).to be_a(Cart)
-        expect(result.data.cart_items.count).to eq(1)
+        expect(subject).to be_success
+        expect(subject.data).to be_a(Cart)
+        expect(subject.data.cart_items.count).to eq(1)
       end
     end
 
     context 'with invalid data' do
+      let(:cart_id) { nil }
+
       context 'when product does not exist' do
         let(:params) { { product_id: 999, quantity: 2 } }
 
         it 'returns error' do
-          result = described_class.call(params)
-          
-          expect(result).not_to be_success
-          expect(result.errors).to be_present
+          expect(subject).not_to be_success
+          expect(subject.errors).to be_present
         end
       end
 
@@ -69,10 +67,8 @@ RSpec.describe Carts::CreatorService do
         let(:params) { { product_id: product.id, quantity: -1 } }
 
         it 'returns error' do
-          result = described_class.call(params)
-          
-          expect(result).not_to be_success
-          expect(result.errors).to be_present
+          expect(subject).not_to be_success
+          expect(subject.errors).to be_present
         end
       end
     end
